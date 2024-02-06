9a9c7c32cfb7a633aae6133eb94eaadedd3840d8
Matheus Alves <matheus.adeoliveira11@gmail.com>
2024-02-06 17:38:36 -0300

    feat: add request body class

---
+++ Communication/Requests/RequestCreateOfferJson.cs
@@ -0,0 +1,9 @@
+﻿namespace RocketseatAuction.Communication.Requests
+{
+    public class RequestCreateOfferJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public float Price { get; set; }
+    }
+}
