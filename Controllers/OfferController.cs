01f5ca2a008c3b027df2739bd0f9460ca6f0fcd9
Matheus Alves <matheus.adeoliveira11@gmail.com>
2024-02-06 17:11:13 -0300

    feat: add offer entity and controller

---
+++ Controllers/OfferController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RocketseatAuction.Controllers
+{
+    public class OfferController : RocketseatAuctionBaseController
+    {
+        [HttpPost]
+        public IActionResult CreateOffer([FromRoute] int itemId)
+        {
+
+            return Created();
+        }
+    }
+}
---
+++ Entities/Offer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RocketseatAuction.Entities
+{
+    [Table("Offers")]
+    public class Offer
+    {
+        public int Id { get; set; }
+        public string CreatedOn { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public int ItemId { get; set; }
+        public int UserId { get; set; }
+    }
+}
