93a843d5f7907ee7ecedc7fe106b55fc467b4787
Matheus Alves <matheus.adeoliveira11@gmail.com>
2024-02-06 17:38:58 -0300

    feat: add bearer token authentication

--- Controllers/OfferController.cs
+++ Controllers/OfferController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.Entities;
+using RocketseatAuction.Filters;
+using RocketseatAuction.UseCases.Items.Get;
 
 namespace RocketseatAuction.Controllers
 {
     public class OfferController : RocketseatAuctionBaseController
     {
-        [HttpPost]
+        [HttpPost("{itemId}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ServiceFilter(typeof(AuthenticationUserAttribute))]
         public IActionResult CreateOffer([FromRoute] int itemId)
         {
+            var useCase = new GetItemUseCase();
+
+            Item? item = useCase.Execute(itemId);
+
+            if (item is null)
+                return NotFound();
 
             return Created();
         }
---
+++ Filters/AuthenticationUserAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RocketseatAuction.Filters
+{
+    public class AuthenticationUserAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            string token = GetTokenOnRequest(context.HttpContext);
+        }
+
+        private string GetTokenOnRequest(HttpContext context)
+        {
+            var authorization = context.Request.Headers.Authorization.ToString();
+
+            return authorization["Bearer ".Length..];
+        }
+    }
+}
