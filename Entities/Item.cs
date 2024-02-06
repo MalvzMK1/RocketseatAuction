5a990c69626c4ba6a1e0f01670f2d84dabcba2ca
Matheus Alves <matheus.adeoliveira11@gmail.com>
2024-02-06 16:55:45 -0300

    feat: add condition enum

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using RocketseatAuction.Enums;
 
 namespace RocketseatAuction.Entities
 {
@@ -8,7 +9,7 @@ namespace RocketseatAuction.Entities
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Brand { get; set; } = string.Empty;
-        public int Condition { get; set; }
+        public Condition Condition { get; set; }
         public float BasePrice { get; set; }
         public int AuctionId { get; set; }
     }
