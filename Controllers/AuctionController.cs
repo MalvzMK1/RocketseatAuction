b5633069d9b06707af3e7b11b85311ea22793ff4
Matheus Alves <matheus.adeoliveira11@gmail.com>
2024-02-06 16:42:05 -0300

    docs: add response types to get current auction route

--- Controllers/AuctionController.cs
+++ Controllers/AuctionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.Entities;
 using RocketseatAuction.UseCases.Auctions.GetCurrent;
 
 namespace RocketseatAuction.Controllers
@@ -9,11 +10,16 @@ namespace RocketseatAuction.Controllers
     public class AuctionController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult getCurrentAuction()
         {
             var useCase = new GetCurrentAuctionsUseCase();
 
             var result = useCase.Execute();
+
+            if (result is null)
+                return NoContent();
 
             return Ok(result);
         }
